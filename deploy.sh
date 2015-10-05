f2df5f1d76396a31d1156dfb44c9f4585e743ea5
Sourabh Shirhatti <shirhatti@outlook.com>
2015-10-05 14:03:42 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,26 @@
-sudo apt-get install -qq nginx
+# Install mono
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+sudo apt-get update
+sudo apt-get install -qq mono-complete
+
+# Install dependencies
+sudo apt-get install -qq unzip libunwind8 gettext libssl-dev libcurl3-dev zlib1g
+
+# Install DNVM
+curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh
+source ~/.dnx/dnvm/dnvm.sh
+
+# Install a global DNX using DNVM
+echo "y" | dnvm install 1.0.0-beta7 -r mono -p -g
+
+# Build LibUV from source
+sudo apt-get install -qq make automake libtool curl
+curl -sSL https://github.com/libuv/libuv/archive/v1.4.2.tar.gz | sudo tar zxf - -C /usr/local/src
+cd /usr/local/src/libuv-1.4.2
+sudo sh autogen.sh
+sudo ./configure
+sudo make
+sudo make install
+sudo rm -rf /usr/local/src/libuv-1.4.2 && cd ~/
+sudo ldconfig
