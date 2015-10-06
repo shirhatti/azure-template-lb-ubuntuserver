e830200695756bb69b98ec2ef5e966a128e01384
Sourabh Shirhatti <shirhatti@outlook.com>
2015-10-06 12:45:06 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,6 @@ sudo apt-get install -qq mono-complete
 # Install dependencies
 sudo apt-get install -qq unzip libunwind8 gettext libssl-dev libcurl3-dev zlib1g
 
-# Install DNVM
-curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh
-source ~/.dnx/dnvm/dnvm.sh
-
-# Install a global DNX using DNVM
-echo "y" | dnvm install 1.0.0-beta7 -r mono -p -g
-
 # Build LibUV from source
 sudo apt-get install -qq make automake libtool curl
 curl -sSL https://github.com/libuv/libuv/archive/v1.4.2.tar.gz | sudo tar zxf - -C /usr/local/src
@@ -24,3 +17,7 @@ sudo make
 sudo make install
 sudo rm -rf /usr/local/src/libuv-1.4.2 && cd ~/
 sudo ldconfig
+
+# Install/configure Nginx
+sudo apt-get install -qq nginx
+curl https://raw.githubusercontent.com/shirhatti/azure-template-lb-ubuntuserver/master/resources/nginx/default | sudo dd of=/etc/nginx/sites-available/default
